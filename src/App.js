efe3c2bad5acc7402c8969be0e6b12685cfa238b
westshine <weisiang0320030207@gmail.com>
2025-10-02 15:22:30 +0800

    update the animation to achieve seamless repeatation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./style.scss";
+import style from "./style.module.scss";
 
 function App() {
   return (
-    <div className="background">
+    <div className={style.background}>
       {[1, 2, 3, 4, 5].map((i) => (
-        <div key={i} className={[`starLayer${i}`]} />
+        <>
+          <div key={i} className={style[`starLayer${i}`]} />
+          <div key={i} className={style[`starLayer${i}Duplicate`]} />
+        </>
       ))}
-      <h1 className="title">Starlit Sky</h1>
+      <h1 className={style.title}>Starlit Sky</h1>
     </div>
   );
 }
